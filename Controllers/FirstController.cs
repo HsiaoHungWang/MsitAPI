b706a55e99d61b20e6e6d9aa13c0895f9aebdcdb
Steven <steven@ispan.com.tw>
2025-07-24 12:01:07 +0800

    接收QueryString的資料

--- Controllers/FirstController.cs
+++ Controllers/FirstController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MsitAPI.Models;
+using MsitAPI.Models.Dto;
 
 namespace MsitAPI.Controllers
 {
@@ -16,18 +17,21 @@ namespace MsitAPI.Controllers
         
 
         [HttpGet("text")]
-    
-        public IActionResult GetText()
-        {
-            //System.Threading.Thread.Sleep(5000);
-            string message = "Hello 中文!!";
+        //?name=Tom&age=20&email=Tom@company.com&phone=123444
+        //public IActionResult GetText(int age, string name)
+
+		public IActionResult GetText([FromQuery]MyDto myDto)
+		{
+           // System.Threading.Thread.Sleep(5000);
+
+            string message = $"Hello {myDto.Name}, You are {myDto.Age} years old.";
             return Content(message, "text/plain", System.Text.Encoding.UTF8);
         }
 
         [HttpGet("image")]
         public IActionResult GetImage1()
         {
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "dog1.jpg");
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "avatars", "cat1.jpg");
             var imageBytes = System.IO.File.ReadAllBytes(filePath);
             return File(imageBytes, "image/jpeg");
         }
